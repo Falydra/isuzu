f91da85a09c4f57ac0dccc6572ec63f335eefe88
Falydra <daffa.khususedukasi@gmail.com>
2024-03-11 14:22:32 +0700

    fixed load env

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,34 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:isuzu/components/splash.dart';
 import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
-  await Supabase.initialize(
-    url: 'https://dcvbcptpncktkldscxmf.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdmJjcHRwbmNrdGtsZHNjeG1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5NjIwMTIsImV4cCI6MjAyNTUzODAxMn0.IdrVMHLGNSCJF4MEuQOGI7TmWShcA31K_H5y9hXG2PU',
-  );
-  runApp(MyApp());
+  final String supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  final String anonKey = dotenv.env['SUPABASE_ANON_KEY']!;
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: anonKey);
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashWrapper(),
-      color: Color(0xFF202525),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class SplashWrapper extends StatefulWidget {
-  @override
-  _SplashWrapperState createState() => _SplashWrapperState();
-}
-
-class _SplashWrapperState extends State<SplashWrapper> {
-  @override
-  void initState() {
-    super.initState();
-
-    Timer(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen();
+        color: const Color(0xFF202525),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashWrapper(),
+          '/login-page': (context) => LoginPage(
+                controller: TextEditingController(),
+              )
+        });
   }
 }
