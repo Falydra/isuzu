bcecb11af7413048095f8c613b1b7c83c5f671c3
Aly <100826322+Falydra@users.noreply.github.com>
2024-03-12 16:11:47 +0700

    fixed error massage

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -18,3 +18,33 @@ void userAuth(BuildContext context, email, String password) async {
     showAlerts(context, e.toString());
   }
 }
+
+void registerAuth(
+  BuildContext context,
+  String email,
+  String password,
+) async {
+  try {
+    List<String> parts = email.split('@');
+    String username = parts[0];
+    AuthResponse res = await supabase.auth
+        .signUp(email: email, password: password, data: {'username': username});
+    if (res.user != null) {
+      Navigator.pushReplacementNamed(context, '/login-page');
+    }
+  } catch (e) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Error"),
+        content: Text("Excpecteed Error, Try Again"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Ingfo diterima"),
+          ),
+        ],
+      ),
+    );
+  }
+}
